0d62d654b8e7860a9ce9d219f6d55f0aa41fc11d
Radfordhound <Radfordhound@users.noreply.github.com>
2015-04-02 20:08:11 -0500

    Removed debug folder I forgot to delete :|

--- bkp/Main.lua
+++
@@ -1,9 +0,0 @@
---Project Unique!
---The engine behind Love Game Maker
---Currently a major work in progress!
-
-require "Main.libraries.hump.class"
-
-function love.draw()
-    love.graphics.print("test",0,0)
-end
